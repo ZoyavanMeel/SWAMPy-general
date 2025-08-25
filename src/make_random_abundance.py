58fe74966fc1c8c239dc4e241cd096c68da87cc1
ZoyavanMeel <109024871+ZoyavanMeel@users.noreply.github.com>
2025-08-25 11:43:33 +0200

    Create make_random_abundance.py

---
+++ src/make_random_abundance.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import random
+import argparse
+
+
+def change_abundance(abundance_file: str, output_file: str, seed: int) -> None:
+    random.seed(seed)
+    df = pd.read_csv(abundance_file, header=None, sep="\t", index_col=None, names=["genome", "abu"])
+    raw_abus = [random.randint(0, 100) for _ in range(df.shape[0])]
+    norm = sum(raw_abus)
+    df["abu"] = [abu/norm for abu in raw_abus]
+    df.to_csv(output_file, sep="\t", index=False, header=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--abundance_file", "-a")
+    parser.add_argument("--output_file", "-o")
+    parser.add_argument("--seed", "-s")
+    args = parser.parse_args()
+
+    change_abundance(args.abundance_file, args.output_file, int(args.seed))
